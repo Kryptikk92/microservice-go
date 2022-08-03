a9e164f229f355b83bbf4af6ca23fa2e6ecb9c41
Lars Junker <larsjunker92@gmail.com>
2022-08-03 18:37:03 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"crypto/tls"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const message = "Hello World"
+
+var (
+	CertFile    = os.Getenv("CERT_FILE")
+	KeyFile     = os.Getenv("KEY_FILE")
+	ServiceAddr = os.Getenv("SERVICE_ADDR")
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(message))
+		if err != nil {
+			log.Fatalf("error while sending response: %v", err)
+		}
+	})
+
+	srv := NewServer(mux, ServiceAddr)
+
+	log.Println(srv.ListenAndServeTLS(CertFile, KeyFile))
+}
+
+func NewServer(mux *http.ServeMux, addr string) *http.Server {
+	tlsConfig := &tls.Config{
+		// Only use curves which have assembly implementations
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP256,
+			tls.X25519, // Go 1.8 only
+		},
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		},
+	}
+
+	srv := &http.Server{
+		Addr:         addr,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		TLSConfig:    tlsConfig,
+		Handler:      mux,
+	}
+	return srv
+}

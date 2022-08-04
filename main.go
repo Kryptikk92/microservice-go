83d5b5d6247e36e4a8560c3e921957b98b2ce599
Lars Junker <larsjunker92@gmail.com>
2022-08-04 09:49:46 +0200

    refactored server into package

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"crypto/tls"
 	"log"
+	"microservice/server"
 	"net/http"
 	"os"
-	"time"
 )
 
 const message = "Hello World"
@@ -27,36 +26,7 @@ func main() {
 		}
 	})
 
-	srv := NewServer(mux, ServiceAddr)
+	srv := server.New(mux, ServiceAddr)
 
 	log.Println(srv.ListenAndServeTLS(CertFile, KeyFile))
-}
-
-func NewServer(mux *http.ServeMux, addr string) *http.Server {
-	tlsConfig := &tls.Config{
-		// Only use curves which have assembly implementations
-		CurvePreferences: []tls.CurveID{
-			tls.CurveP256,
-			tls.X25519, // Go 1.8 only
-		},
-		MinVersion: tls.VersionTLS12,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		},
-	}
-
-	srv := &http.Server{
-		Addr:         addr,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  120 * time.Second,
-		TLSConfig:    tlsConfig,
-		Handler:      mux,
-	}
-	return srv
 }
